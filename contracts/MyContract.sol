35d8b723a4e82b3bfd29c4c492103bdfc9e20e5f
pprathom <114977014+pprathom@users.noreply.github.com>
2022-10-07 04:25:20 +0700

    Update MyContract.sol
    add comment

--- contracts/MyContract.sol
+++ contracts/MyContract.sol
@@ -12,10 +12,12 @@ constructor(string memory name, uint balance){
     _balance = balance;
 }
 
+// Get balance function
 function getBalance() public view returns(uint myBalance){
     return _balance;    
 }
 
+// deposit function
 function deposit(uint amount) public{
     _balance += amount;    
 }
